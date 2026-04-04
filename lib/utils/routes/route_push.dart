bd223d9c71214f08c949d9679a40b422947018bc
Noor Ullah <nooullah178632@gmail.com>
2026-04-04 22:23:45 +0500

    setup the project

---
+++ lib/utils/routes/route_push.dart
@@ -0,0 +1,6 @@
+class RoutesName {
+  static const String splash = 'splash_view';
+  static const String chatScreen = 'chat_view';
+  static const String imageGenScreen = 'image_gen_view';
+  static const String homeScreen = "home_screen";
+}
---
+++ lib/view/image_generate_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImageGenerateView extends StatefulWidget {
+  const ImageGenerateView({super.key});
+
+  @override
+  State<ImageGenerateView> createState() => _ImageGenerateViewState();
+}
+
+class _ImageGenerateViewState extends State<ImageGenerateView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
