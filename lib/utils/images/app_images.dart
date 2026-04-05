051fd7f7753f0b3cc9423f433ac98bb78f70dd78
Noor Ullah <nooullah178632@gmail.com>
2026-04-05 17:50:38 +0500

    images set

--- lib/utils/images/app_images.dart
+++ lib/utils/images/app_images.dart
@@ -1,3 +1,8 @@
 class AppImages {
+  //splash logo
   static const String splashLogo = 'assets/onBoardingPic/splash_pic.png';
+  //onBoarding logos
+  static const String humanLogo = "assets/onBoardingPic/human_ux_pic.png";
+  static const String chatPic = "assets/onBoardingPic/chat_pic.png";
+  static const String imagePic = "assets/onBoardingPic/image_pic.png";
 }
