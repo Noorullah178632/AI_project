d4887c0d44a774f25f3193c592c59d62e4c2b18c
Noor Ullah <nooullah178632@gmail.com>
2026-04-07 15:39:55 +0500

    splash screen added and screenutils package added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ai_project/utils/routes/route_name.dart';
 import 'package:ai_project/utils/routes/route_push.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -13,12 +14,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter AI Application',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      initialRoute: RoutesName.onBoardindScreen,
-      onGenerateRoute: RoutePush.generateRoute,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter AI Application',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+          ),
+          initialRoute: RoutesName.splash,
+          onGenerateRoute: RoutePush.generateRoute,
+        );
+      },
     );
   }
 }
